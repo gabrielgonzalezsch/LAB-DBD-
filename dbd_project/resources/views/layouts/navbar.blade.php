ce09b63941b0b900a05e336ab6aadeac593982a4
Soul <vicente.rivera.r@usach.cl>
2018-08-01 00:50:32 -0400

    Version 0.0.71 1/8/18

--- dbd_project/resources/views/layouts/navbar.blade.php
+++ dbd_project/resources/views/layouts/navbar.blade.php
@@ -1,66 +1,126 @@
-<style>
-  .col .i{
-    margin-left: 5px;
-    margin-right: 5px;
-    padding: 10px;
-  };
-</style>
-<div class="navigation">
-    <ul id="navbar" class="row">
-      <a id="title" class="col-2" href="/">{{ config('app.name', 'DBD') }}</a>
-      <ul class="col"> <i class="fas fa-sort-down"></i> Vuelos <i class="fas fa-plane"></i>
-        @if(Auth::check())
-        <li><a href="/vuelos/create"> Agregar vuelo</a></li>
-        @endif
-        <li><a href="/vuelos"> Ver vuelos</a></li>
-        <li><a href="#"> Buscar vuelos</a></li>
-      </ul>
-      <ul class="col"> <i class="fas fa-sort-down"></i> Autos <i class="fas fa-car"></i>
-        @if(Auth::check())
-        <li><a href="/autos/create"> Agregar autos</a></li>
-        @endif
-        <li><a href="/autos"> Ver autos</a></li>
-        <li><a href="#"> Buscar autos</a></li>
-      </ul>
-      <ul class="col"> <i class="fas fa-sort-down"></i> Traslados <i class="fas fa-map-marker-alt"></i>
-        @if(Auth::check())
-        <li><a href="#"> Agregar traslados</a></li>
-        @endif
-        <li><a href="#"> Ver traslados</a></li>
-        <li><a href="#"> Buscar traslados</a></li>
-      </ul>
-    <ul class="col"> <i class="fas fa-sort-down"></i> Hoteles <i class="far fa-building"></i>
-      @if(Auth::check())
-      <li><a href="/hoteles/create"> Agregar hoteles</a></li>
+
+<div id="navbar" class="row">
+    <h2 style="margin: 5px !important; width: 20%; float: left;">
+      <a id="logo" href="/">{{ config('app.name', 'DBD') }}</a>
+    </h2>
+    <ul tabindex="1" class="categoria">
+      <div class="titulo-categoria"> <i class="fas fa-sort-down"></i> Vuelos
+        <i class="fas fa-plane"></i>
+      </div>
+      <div class="contenido-categoria">
+      @if(!empty(Auth::user()))
+        <a href="/vuelos/create"> Agregar vuelo</a>
       @endif
-      <li><a href="/hoteles"> Ver hoteles</a></li>
-      <li><a href="#"> Buscar hoteles</a></li>
+      <a href="/vuelos"> Ver vuelos</a>
+      </div>
     </ul>
-    <ul class="col"> <i class="fas fa-sort-down"></i> Actividades <i class="far fa-calendar-alt"></i>
-      @if(Auth::check())
-      <li><a href="#"> Agregar actividades</a></li>
-      @endif
-      <li><a href="#"> Ver actividades</a></li>
-      <li><a href="#"> Buscar actividades</a></li>
+    <ul tabindex="2" class="categoria">
+      <div class="titulo-categoria"> <i class="fas fa-sort-down"></i> Autos
+        <i class="fas fa-car"></i>
+      </div>
+      <div class="contenido-categoria">
+        <a href="/autos/create"> Agregar auto</a>
+        <a href="/autos"> Ver autos</a>
+      </div>
+    </ul>
+    <ul tabindex="3" class="categoria">
+      <div class="titulo-categoria"> <i class="fas fa-sort-down"></i>Traslados
+        <i class="fas fa-map-marker-alt"></i>
+      </div>
+      <div class="contenido-categoria">
+        @if(!empty(Auth::user()))
+          <a href="/cursos/create"> Agregar traslados</a>
+        @endif
+        <a href="/traslados"> Ver Traslados</a>
+      </div>
+    </ul>
+    <ul tabindex="4" class="categoria">
+      <div class="titulo-categoria"> <i class="fas fa-sort-down"></i>Hoteles
+        <i class="far fa-building"></i>
+      </div>
+      <div class="contenido-categoria">
+        @if(Auth::check())
+        <a href="/hoteles/create"> Agregar hoteles</a>
+        @endif
+        <a href="/hoteles"> Ver hoteles</a>
+      </div>
+    </ul>
+   <ul tabindex="5" class="categoria">
+     <div class="titulo-categoria"><i class="fas fa-sort-down"></i>Actividades
+       <i class="far fa-calendar-alt"></i>
+     </div>
+     <div class="contenido-categoria">
+       @if(!empty(Auth::check()))
+        <a href="/actividades/create">Agregar actividad</a>
+       @endif
+       <a href="/actividades"> Ver actividades</a>
+     </div>
     </ul>
     @if(Auth::check())
-    <ul class="col"> <i class="fas fa-sort-down"></i> {{ Auth::user()->username }}  <i class="far fa-user"></i>
-      <li><a href="#"> Historial de compra</a></li>
-      <li><a href="{{ route('logout') }}"
-         onclick="event.preventDefault();
-                  document.getElementById('logout-form').submit();">
-                  {{ __('Salir') }}
-          </a>
-          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-              @csrf
-          </form>
-      </li>
+    <ul tabindex="7" class="categoria">
+      <div class="titulo-categoria">{{ Auth::user()->name }}<i class="fas fa-sort-down"></i>
+      </div>
+      <div class="contenido-categoria">
+        <a href="#"> Historial de compra</a>
+        <a href="#"> Carrito de compra</a>
+        @if(Auth::check())
+        <a href="/admin"> Administración </a>
+        @endif
+        <a href="{{ route('logout') }}"
+           onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit();">
+                    {{ __('Salir') }}
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+      </div>
     </ul>
     @else
-    <ul class="col"> <i class="fas fa-sort-down"></i> Ingresar al sitio <i class="far fa-user"></i>
-      <li><a href="/login"> Ingresar</a></li>
-      <li><a href="/register"> Registrarse</a></li>
+    <ul tabindex="7" class="categoria">
+      <div class="titulo-categoria"> <i class="fas fa-sort-down"></i>Ingresar al sitio
+      </div>
+      <div class="contenido-categoria">
+        <a href="/login"> Ingresar</a>
+        <a href="/register"> Registrarse</a>
+      </div>
     </ul>
     @endif
-  </ul>
+    <div class="menu-wrapper">
+      <i class="bars icon"> Menú</i>
+    </div>
 </div>
+<style>
+  @media only screen and (min-width: 1030px) {
+      .categoria{
+        display: block;
+        width: calc(30px + (170 - 30) * ((100vw - 300px) / (1600 - 300)));
+      }
+      .titulo-categoria{
+        font-size: calc(9px + (16 - 9) * ((100vw - 300px) / (1600 - 300)));
+      }
+      .menu-wrapper{
+        display: none;
+      }
+  }
+  @media only screen and (max-width: 1030px) {
+      #navbar .categoria{
+        display: none;
+      }
+      .menu-wrapper{
+        display: block !important;
+      }
+  }
+
+  .menu-wrapper{
+    display: none;
+    font-size: 16px;
+    margin: 10px 30px 0px 0px;
+    max-width: 50px;
+  }
+  .categoria i{
+    margin-top: -10px;
+    padding-top: 0;
+  }
+
+</style>
